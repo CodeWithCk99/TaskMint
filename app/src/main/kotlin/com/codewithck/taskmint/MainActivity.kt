9d6c4fa179f0dd6cec12428c771357e227bb96f8
CodeWithCk99 <codewithck99@gmail.com>
2026-07-03 07:25:07 +0530

    Connect floating action button

--- app/src/main/kotlin/com/codewithck/taskmint/MainActivity.kt
+++ app/src/main/kotlin/com/codewithck/taskmint/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.codewithck.taskmint
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fabAdd = findViewById<FloatingActionButton>(R.id.fabAdd)
+
+        fabAdd.setOnClickListener {
+            Toast.makeText(this, "Add Task Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
